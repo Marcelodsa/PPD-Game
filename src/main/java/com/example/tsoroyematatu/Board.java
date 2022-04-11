d8d515418e85dfea94e3d0d9b5234f488d4be10e
Marcelo de Sousa Alves <marcelo_sousa@virgo.atlantico.net.br>
2022-04-11 18:58:38 -0300

    Finish

--- src/main/java/com/example/tsoroyematatu/Board.java
+++
@@ -1,7 +0,0 @@
-package com.example.tsoroyematatu;
-
-import javafx.scene.shape.Circle;
-
-public class Board extends Circle {
-
-}
---
+++ src/main/java/com/example/tsoroyematatu/Launcher.java
@@ -0,0 +1,2 @@
+package com.example.tsoroyematatu;public class Launcher {
+}
